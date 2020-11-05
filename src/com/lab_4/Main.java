ad11db7104152a68539ed0a035379a71ee1662bf
Кирилл <skynet0887@gmail.com>
2020-11-05 22:17:54 +0700

    interview

--- src/com/lab_4/Main.java
+++ src/com/lab_4/Main.java
@@ -1,8 +1,18 @@
 package com.lab_4;
 
+import java.util.Scanner;
+
 public class Main {
 
+    public static int summ(int a, int b){
+        return a+b;
+    }
+
     public static void main(String[] args) {
-	System.out.println("Hello World");
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(summ(a, b));
+
     }
 }
