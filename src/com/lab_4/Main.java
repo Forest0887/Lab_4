aa7a05ac46a5b86e57db63e68accfd25aceef47b
Кирилл <skynet0887@gmail.com>
2020-11-07 16:45:48 +0700

    Chempionat last ver

--- src/com/lab_4/Main.java
+++ src/com/lab_4/Main.java
@@ -1,18 +1,25 @@
 package com.lab_4;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        int a = 6111;
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        boolean asd = false;
         for (int i = 2; i < 10; i++){
             for (int j = 2; j < 10; j++){
                 String c = i + Integer.toString(j);
                 int b = Integer.parseInt(c);
                 if (i*j*(b) == a){
-                    System.out.println(i);
-                    System.out.println(j);
+                    System.out.print(b);
+                    asd = true;
                 }
             }
+        }
+        if (!asd){
+            System.out.println("ERROR");
         }
     }
 }
